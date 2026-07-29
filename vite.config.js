26276f22f9bc6b0d34caa801b0d792b1acfe77b5
ayush0419-lab <ayush0419lab@gmail.com>
2026-07-29 23:37:24 +0530

    Dynamically set build base path for GitHub Actions vs Vercel/Local

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CV-Generator-/',
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/CV-Generator-/' : '/',
 })
+
 
